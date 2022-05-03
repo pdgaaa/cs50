61a5d9b02739b7fbea2e28e2d2649b348163b78d
pdgaaa <pdg@gatouillat.net>
2022-05-03 22:51:23 +0200

    update readability - good version

--- readability/readability.c
+++ readability/readability.c
@@ -18,7 +18,20 @@ int main(void)
     float L = ((float) number_letters/(float) number_words)*100;
     float S = ((float) number_sentences/(float) number_words)*100;
     float index = 0.0588 * L - 0.296*S - 15.8;
-    printf("L is %f, S is %f, Grade %d\n", L, S, (int) round(index));
+    int grade = (int) round(index);
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+
+    }
+    else
+    {
+        printf("Grade %d\n", grade);
+    }
 }
 
 int count_sentences(string text)
@@ -37,7 +50,7 @@ int count_sentences(string text)
 }
 int count_words(string text)
 {
-   //start at 1 cause we can't detect the first or last word
+   //start at 1 cause we can't detect the first word
    int count = 1;
    for (int i = 0, n = strlen(text); i < n; i++)
    {
