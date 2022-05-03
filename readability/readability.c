b5faddac4938a00bb94481fab441796eb837915d
pdgaaa <pdg@gatouillat.net>
2022-05-03 14:13:56 +0200

    update readability

--- readability/readability.c
+++ readability/readability.c
@@ -2,31 +2,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
 int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void) 
 {
     string text = get_string("Text: ");
     int number_letters = count_letters(text);
     int number_words = count_words(text);
-    printf("%i letters\n %i words\n", number_letters, number_words);
+    int number_sentences = count_sentences(text);
+    printf("%i letters\n %i words\n %i sentences\n", number_letters, number_words, number_sentences);
+    float L = ((float) number_letters/(float) number_words)*100;
+    float S = ((float) number_sentences/(float) number_words)*100;
+    float index = 0.0588 * L - 0.296*S - 15.8;
+    printf("L is %f, S is %f, Grade %d\n", L, S, (int) round(index));
 }
 
-int count_words(string text)
+int count_sentences(string text)
 {
    int count = 0;
    for (int i = 0, n = strlen(text); i < n; i++)
    {
-       char letter = text[i];
+       char character = text[i];
+       //. is 46, ! is 33, ? is 63
+       if ((character == 46) || (character == 33) || (character == 63))
+       {
+           count++;
+       }
+   }
+   return count;
+}
+int count_words(string text)
+{
+   //start at 1 cause we can't detect the first or last word
+   int count = 1;
+   for (int i = 0, n = strlen(text); i < n; i++)
+   {
        //space char == 32
        if (isalpha(text[i]) && text[i-1] == 32)
        {
            count++;
        }
    }
-    return count;
+   return count;
 }
 
 int count_letters(string text)
@@ -42,4 +63,3 @@ int count_letters(string text)
    }
    return count;
 }
-
