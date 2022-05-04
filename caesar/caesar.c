bbbfdd6535a492195dd6db5b97edd0edb2a0d892
pdgaaa <pdg@gatouillat.net>
2022-05-04 14:04:34 +0200

    caesar pseudocode

---
+++ caesar/caesar.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[]) 
+{
+    //pseudocode
+    //get the key
+    /*int positive k = argv[1];
+    */
+    /*if no argument on command line
+        print error message
+        exit and return 1
+    */
+    /*if argument is not a decimal digit
+        print Usage: ./caesar key
+        exit and return 1
+    */
+    /*if argument k > 26
+        key = k % 26
+        if key == 0
+            k = 26
+    */
+}
