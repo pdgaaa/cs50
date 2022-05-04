3582a4a0212ebd8a0aa67eb760bb5a7270aa8074
pdgaaa <pdg@gatouillat.net>
2022-05-04 13:36:21 +0200

    indentation correction

--- readability/readability.c
+++ readability/readability.c
@@ -14,10 +14,10 @@ int main(void)
     int number_letters = count_letters(text);
     int number_words = count_words(text);
     int number_sentences = count_sentences(text);
-    printf("%i letters\n %i words\n %i sentences\n", number_letters, number_words, number_sentences);
-    float L = ((float) number_letters/(float) number_words)*100;
-    float S = ((float) number_sentences/(float) number_words)*100;
-    float index = 0.0588 * L - 0.296*S - 15.8;
+    //printf("%i letters\n %i words\n %i sentences\n", number_letters, number_words, number_sentences);
+    float L = ((float) number_letters / (float) number_words) * 100;
+    float S = ((float) number_sentences / (float) number_words) * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
     int grade = (int) round(index);
     if (grade < 1)
     {
@@ -36,43 +36,43 @@ int main(void)
 
 int count_sentences(string text)
 {
-   int count = 0;
-   for (int i = 0, n = strlen(text); i < n; i++)
-   {
-       char character = text[i];
-       //. is 46, ! is 33, ? is 63
-       if ((character == 46) || (character == 33) || (character == 63))
-       {
-           count++;
-       }
-   }
-   return count;
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        char character = text[i];
+        //. is 46, ! is 33, ? is 63
+        if ((character == 46) || (character == 33) || (character == 63))
+        {
+            count++;
+        }
+    }
+    return count;
 }
 int count_words(string text)
 {
-   //start at 1 cause we can't detect the first word
-   int count = 1;
-   for (int i = 0, n = strlen(text); i < n; i++)
-   {
-       //space char == 32
-       if (isalpha(text[i]) && text[i-1] == 32)
-       {
-           count++;
-       }
-   }
-   return count;
+    //start at 1 cause we can't detect the first word
+    int count = 1;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        //space char == 32
+        if (isalpha(text[i]) && text[i - 1] == 32)
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int count_letters(string text)
 {
-   int count = 0;
-   for (int i = 0, n = strlen(text); i < n; i++)
-   {
-      char letter = text[i];
-      if ((letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122))
-      {
-        count++;
-      }
-   }
-   return count;
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        char letter = text[i];
+        if ((letter >= 65 && letter <= 90) || (letter >= 97 && letter <= 122))
+        {
+            count++;
+        }
+    }
+    return count;
 }
