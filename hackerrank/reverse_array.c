aac8a52e69a7ad7f4c2ad88e1b8d3101efce10f2
pdgaaa <pdg@gatouillat.net>
2022-05-17 13:48:57 +0200

    hackerrank init

---
+++ hackerrank/reverse_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+
+    /* Write the logic to reverse the array. */
+    for(i = 0; i < (num / 2); i++)
+    {
+        int first = arr[i];
+        int last = arr[num - 1 - i];
+        arr[i] = last;
+        arr[num - 1 - i] = first;
+    }
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
