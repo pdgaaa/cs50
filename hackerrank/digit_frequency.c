d0d425b4ab85c0e665414c9a207c2c15ef0fbf4c
pdgaaa <pdgaaa@users.noreply.github.com>
2022-05-18 13:03:24 +0200

    Update digit_frequency.c

--- hackerrank/digit_frequency.c
+++ hackerrank/digit_frequency.c
@@ -2,13 +2,15 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    char *s;
-    s = malloc(1024 * sizeof(char));
+    //char *s;
+    //s = malloc(1024 * sizeof(char));
     //char *counting_array;
+    char s[1024];
     //counting_array = malloc(1024 * sizeof(char));
     //init an array with 0
     int counting_array[1024];
@@ -19,22 +21,25 @@ int main() {
     
     //get input
     scanf("%[^\n]", s);
-    s = realloc(s, strlen(s) + 1);
+    //s = realloc(s, strlen(s) + 1);
 
     //count digit
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         if (isdigit(s[i]))
         {
-            counting_array[s[i]] += 1;
+            char digit = s[i];
+//            int digit = atoi(ascii_digit);
+            counting_array[digit] += 1;
+            printf("%i\n", counting_array[digit]);
         }
 
     }
     
     //print digit frequency
-    for (int i = 0, n = strlen(s); i < n; i++)
+    for (int i = 0; i < 10; i++)
     {
-        
+        printf("%i ", counting_array[i]);
     }
     return 0;
 }
