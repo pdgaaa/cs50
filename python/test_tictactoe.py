4bc606bfea4e09893b4ffe0426d1b959965590f5
pdgaaa <pdg@gatouillat.net>
2023-03-15 21:38:35 +0100

    check winner tictactoe

---
+++ python/test_tictactoe.py
@@ -0,0 +1,49 @@
+from tictactoe import winner
+
+def test_winner_line_top():
+    board=[
+        ["X","X","X"],
+        ["*","*","*"],
+        ["*","*","*"]
+    ]
+    assert winner(board) == "X"
+
+def test_winner_middle_line():
+    board=[
+        ["*","*","*"],
+        ["O","O","O"],
+        ["*","*","*"]
+    ]
+    assert winner(board) == "O"
+
+def test_winner_bottom_line():
+    board=[
+        ["*","*","*"],
+        ["*","*","*"],
+        ["X","X","X"]
+    ]
+    assert winner(board) == "X"
+
+def test_winner_diagonal_slash():
+    board=[
+        ["X","*","O"],
+        ["*","O","*"],
+        ["O","*","X"]
+    ]
+    assert winner(board) == "O"
+
+def test_winner_middle_column():
+    board=[
+        ["O","X","*"],
+        ["O","X","*"],
+        ["*","X","O"]
+    ]
+    assert winner(board) == "X"
+
+def test_winner_diagonal_backslash():
+    board=[
+        ["X","*","*"],
+        ["*","X","*"],
+        ["*","*","X"]
+    ]
+    assert winner(board) == "X"
--- python/tictactoe.py
+++ python/tictactoe.py
@@ -42,40 +42,50 @@ def check_occupied(line,column):
     return False
 
 # fonction pour check si y'a un gagnant
+# pb : dans mon board initialise à *, * va être gagnant !
 def winner(board):
     #check lines
-    if board[0][0] == board[0][1] == board[0][2]: winner() & exit
-    elif board[1][0] == board[1][1] == board[1][2]: winner() & exit
-    elif board[2][0] == board[2][1] == board[2][2]: winner() & exit
-    #column
-    elif board[0][0] == board[1][0] == board[2][0]: winner() & exit
-    elif board[0][1] == board[1][1] == board[2][1]: winner() & exit
-    elif board[0][2] == board[1][2] == board[2][2]: winner() & exit
-
     for line in range(3):
-        for column in range(3):
-            if board[line][column] != board[line][column+1]: return
-            #on continue avec le suivant, de proche en proche
-            #inconvénient on lit 2 fois le même element
+        line_all_equal = True
+        for column in range(2):
+            if board[line][column] != board[line][column+1]:
+                line_all_equal = False
+                break
+        if line_all_equal and board[line][column] != "*":
+            return board[line][column]
 
-
-
-            if board[line][column] == board[line][column] == board[line][column]:
-                print("winner is " + user)
-                return
-    #check columns
+    #check column
     for column in range(3):
-        for line in range(3):
-            if board[column][line] == board[column][line] == board[column][line]:
-                print("winner is " + user)
-                return
-    #check diagonale
-    if board[0][0] == board[1][1] == board[2][2]:
-        print("winner is " + user)
-        return
-    elif board[2][0] == board[1][1] == board[0][2]:
-        print("winner is " + user)
-        return
+        column_all_equal = True
+        for line in range(2):
+            if board[line][column] != board[line+1][column]:
+                column_all_equal = False
+                break
+        if column_all_equal and board[line][column] != "*":
+            return board[line][column]
+
+    #check diagonal \
+    diagonal_all_equal = True
+    for column in range(2):
+        line = column
+        if board[line][column] != board[line+1][column+1]:
+            diagonal_all_equal = False
+            break
+    if diagonal_all_equal and board[line][column] != "*":
+        return board[line][column]
+
+    #check diagonal /
+    diagonal_all_equal = True
+    for line in range(2):
+        column = 2-line
+        if board[line][column] != board[line+1][column-1]:
+            diagonal_all_equal = False
+            break
+    if diagonal_all_equal and board[line][column] != "*":
+        return board[line][column]
+
+    #no winner
+    return ""
 
 # fonction pour sauvegarder la partie dans un fichier
 def backup():
@@ -83,16 +93,20 @@ def backup():
 
 tour = 0
 
-while tour < 9:
-    if tour % 2 == 0:
-        user = "X"
-    else:
-        user = "O"
+if __name__ == "__main__":
+    while tour < 9:
+        if tour % 2 == 0:
+            user = "X"
+        else:
+            user = "O"
 
-    line, column = ask_user(user)
-    while check_occupied(line, column):
         line, column = ask_user(user)
-    board[line][column] = user
-    print_board(board)
-    winner(board)
-    tour += 1
+        while check_occupied(line, column):
+            line, column = ask_user(user)
+        board[line][column] = user
+        print_board(board)
+        winner_user = winner(board)
+        if winner_user != "":
+            print("winner is " + winner_user)
+            break
+        tour += 1
